0c8fe04de396eff15408734a32520e0624433994
repl.it user <replituser@example.com>
2020-10-22 11:56:47 +0000

    javascripted function of calculate input and css of buttons

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function calculate(num) {
   document.getElementById("inputNo").value = document.getElementById("inputNo").value + num;
 }
+function calculatingInput() {
+  document.getElementById("inputNo").value = eval(document.getElementById("inputNo").value);
+}
