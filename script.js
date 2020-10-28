6d1cb31f0815373f80249f25a52fefc19b3acb6f
repl.it user <replituser@example.com>
2020-10-28 09:32:26 +0000

    deleted css and javascript file and merge them into index.html

--- script.js
+++
@@ -1,6 +0,0 @@
-function calculate(num) {
-  document.getElementById("inputNo").value = document.getElementById("inputNo").value + num;
-}
-function calculatingInput() {
-  document.getElementById("inputNo").value = eval(document.getElementById("inputNo").value);
-}
