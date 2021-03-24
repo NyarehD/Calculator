a26f45c018ebb5ce57d6d3a0c3860493edb97e23
NyarehD <nyarehdavid56@gmail.com>
2021-03-24 13:42:08 +0630

    Finished darkmode but I want to add more color while changing nightmode

--- index.js
+++ index.js
@@ -27,3 +27,31 @@ function cls(){
 function calculating(){
     input.value = eval(input.value);
 }
+
+// Night mode function
+
+const nightModeSwitch = document.getElementById("nightMode");
+let nightMode = false;
+nightModeSwitch.addEventListener("change", ()=>{
+    const bodyBackground = document.querySelector("body");
+    const calculatorBackground = document.getElementById("buttons");
+    const inputNo = document.getElementById("inputNo");
+    const header = document.querySelector("h1");
+    if(nightMode){
+        // To daymode from nightmode
+        console.log(`To daymode from nightmode ${nightMode}`);
+        nightMode = false;
+        bodyBackground.style.backgroundColor = "#ffd60a";
+        calculatorBackground.style.backgroundColor = "#fac720";
+        inputNo.style.backgroundColor = "rgb(255, 255, 255)";
+        header.style.color = "rgb(49, 49, 49)";
+    }else{
+        // To nightmode from daymode
+        console.log(`To nightmode from daymode ${nightMode}`);
+        nightMode = true;
+        bodyBackground.style.backgroundColor = "rgb(31, 31, 31)";
+        calculatorBackground.style.backgroundColor = "#3d3d3bbd";
+        
+        header.style.color = "goldenrod";
+    }
+})
